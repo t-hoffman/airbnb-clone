722ba60ebc14e90357b93fb346b1773b0a824bd2
ShadowTorn33 <austincheer94@gmail.com>
2023-01-05 11:36:26 -0800

    Updated Footer Component

---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p><FontAwesomeIcon icon="fa-thin fa-copyright" /> CommandoLando,Inc.</p>
+        <div>.</div>
+        <p>Terms</p>
+        <div>.</div>
+        <p>Sitemap</p>
+        <div>.</div>
+        <p>Privacy</p>
+        <div>.</div>
+        <p>Your Privacy Choices</p>
+        <div>.</div>
+        <p>Destinations</p>
+    </div>
+  )
+}
+
+export default Footer
