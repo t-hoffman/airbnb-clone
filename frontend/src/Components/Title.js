36860ae15b14d2a1395e58a7e7028e763e6bd51f
TimisCarrington <timiscarrington@gmail.com>
2023-01-06 13:33:59 -0800

    put most info on listing page, still needs styling and formatting

--- frontend/src/Components/Title.js
+++ frontend/src/Components/Title.js
@@ -1,4 +1,6 @@
 import React from "react";
+import * as Icons from 'react-icons/fa'
+import {FiShare} from 'react-icons/fi'
 
 //Title of listing page (under header)
 
@@ -7,11 +9,11 @@ const Title = (props) => {
     return (
       <div className="container">
         <h1>{props.name}</h1>
-        <span>
-         
-        </span>
-        <span>{}</span>
-        <span>{}</span>
+        <div><Icons.FaStar size={20} />{props.stars}</div>
+        <div>{props.review}reviews</div>
+        <div>{props.address}</div>
+        <div><FiShare /> Share</div>
+        <div><Icons.FaRegHeart/> Save</div>
       </div>
     );
   };
