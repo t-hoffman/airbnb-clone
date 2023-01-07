c607e027edf267035f986fba7d297f3ef56b2f44
ShadowTorn33 <austincheer94@gmail.com>
2023-01-07 13:22:07 -0800

    Added some comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,19 +9,22 @@ import { Routes, Route } from "react-router-dom";
 export default function App() {
     const [user, setUser] = useState({})
 
-  //This is basically telling react that google does exist even though its not defined anywhere. So it will ignore errors having to do with defining google.
+  // res in this case is the JWT. Line 15 turns the unreadable code into an object. Line 17 will hide the button when logged in.
   function handleCallbackResponse(res) {
     console.log("Encoded JWT ID Token:" + res.credential);
     const userObject = jwt_decode(res.credential)
     console.log(userObject)
+    setUser(userObject)
     document.getElementById("signInDiv").hidden = true
   }
 
+  // Using state as the cache so we put setUser in here. empty object is saying if user is an empty object there is no one signed in.
   function handleSignOut(e) {
     setUser({})
     document.getElementById("signInDiv").hidden = false
   }
 
+  //This is basically telling react that google does exist even though its not defined anywhere. So it will ignore errors having to do with defining google. Corilated with index.html in public.
   useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
@@ -29,18 +32,24 @@ export default function App() {
       callback: handleCallbackResponse,
     });
 
+    // This is whats rendering the google button and is also targetting out div as the button
     google.accounts.id.renderButton(
         document.getElementById("signInDiv"),
         { theme: "outline", size: "large"}
     );
 
+    // This line prompts u on page load in the upper right
     google.accounts.id.prompt()
   }, []);
 
   return (
     <>
+    {/* This is the sign in button div */}
     <div id="signInDiv"></div>
+    {/* This means that our user acutally has full user attributes. Which means we havea  user that is logged in. So we put log out button inside this conditional */}
+    { Object.keys(user).length != 0 && 
     <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+    }
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/:id" element={<ListingPage />} />
