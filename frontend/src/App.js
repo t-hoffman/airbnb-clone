28e5f80650e671230d0c677b11d0783141612a59
Tyler Hoffman <tjhoffmn@gmail.com>
2023-01-07 20:46:54 -0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,32 +57,4 @@ export default function App() {
       </Routes>
     </>
   );
-=======
-import React from "react"
-import SearchBar from "Components/SearchBar"
-import LandingPage from "Pages/LandingPage"
-import {Routes, Route, useParams} from 'react-router-dom'
-import './Assets/index.css'
-
-export default function App() {
-  const params = Object.keys(useParams()),
-        paramCheck = params.length ? true : false,
-        divClass = params.length ? 'abnb-list-container' : 'abnb-container';
-
-  return(
-    <>
-      {
-        !paramCheck && <SearchBar />
-      }
-      <div className={divClass}>
-        {
-          paramCheck && <SearchBar />
-        }
-        <Routes>
-          <Route path='/' element= {<LandingPage />} />
-        </Routes>
-      </div>
-    </>
-  )
->>>>>>> tyler
 }
