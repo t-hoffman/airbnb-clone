8186026f6bbd3aebf378ef6fd6441a360b9adf0a
TimisCarrington <timiscarrington@gmail.com>
2023-01-10 21:10:00 -0800

    added a map with icons, that show listing img, and title, with a link to the listing page

--- frontend/src/Pages/Search.js
+++ frontend/src/Pages/Search.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Cards from 'Components/Cards';
 import { useParams } from 'react-router-dom';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import { Link } from 'react-router-dom';
 
 const Search = () => {
   const { query } = useParams();
   const [data, setData] = useState(null);
+  const [multipleMarkers, setMutipleMarkers] = useState(null)
 
   const fetchData = async () => {
     const resp = await fetch(`/home/location/${query}?limit=50`);
@@ -12,9 +15,30 @@ const Search = () => {
     setData(json);
   }
 
+  const getMarkers = () => {
+    if (data) {
+      const allMarkers = data.map((home, idx) => {
+        if (idx < 20) {
+          let coordinates = [home.location.lat, home.location.long];
+          return (<Marker key={idx} position={coordinates} >
+            <Popup><Link to={`/listing/${home._id}`}><img style={{ width: '100%' }} src={home.photos[0]} /></Link>
+              <br />
+              {home.name}</Popup>
+          </Marker>)
+
+        }
+      })
+      setMutipleMarkers(allMarkers)
+    }
+  }
+
   useEffect(() => {
     fetchData();
   }, [query]);
+
+  useEffect(() => {
+    getMarkers()
+  }, [data]);
 
   const loaded = () => {
     console.log(data)
@@ -25,6 +49,14 @@ const Search = () => {
         <div className="abnb-main-cont mt-5">
           <div className="main-list">
             <Cards data={data} /> 
+            <MapContainer center={data ? [data[0].location.lat, data[0].location.long] : ''} zoom={10} scrollWheelZoom={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {multipleMarkers}
+
+          </MapContainer>
           </div>
           <div className="text-center mt-3">
             Pages | 1 2 3 4
