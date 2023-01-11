085f3b1f09d02fc27cbcdf33ad92a3edc910eb77
TimisCarrington <timiscarrington@gmail.com>
2023-01-11 08:02:34 -0800

    just added the stars and the rate to the map cards

--- frontend/src/Pages/Search.js
+++ frontend/src/Pages/Search.js
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import { Link } from 'react-router-dom';
 
+
 const Markers = (props) => {
   const { data } = props;
   const map = useMap();
@@ -17,13 +18,22 @@ const Markers = (props) => {
 
   if (data) {
     const allMarkers = data.map((home, idx) => {
+      console.log(home)
       if (idx < 16) {
         let coordinates = [home.location.lat, home.location.long];
         return (
           <Marker position={coordinates} key={idx}>
             <Popup>
               <Link to={`/listing/${home._id}`}><img style={{ width: '100%' }} src={home.photos[0]} key={idx} /></Link><br />
-              {home.name}
+              <h6>{home.name}</h6>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div style={{ marginRight: 'auto' }} >
+                  <div style={{ width: '60px' }}><i className="fa fa-star"></i> {home.stars}</div>
+                </div>
+                <div style={{ marginLeft: 'auto' }}>
+                  <span>${home.rate}</span>
+                </div>
+              </div>
             </Popup>
           </Marker>
         )
