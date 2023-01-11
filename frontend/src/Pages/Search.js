fc77db566088e35744c522090fbfcd276e526ccd
Tyler Hoffman <tjhoffmn@gmail.com>
2023-01-10 22:17:18 -0800

    pagination complete

--- frontend/src/Pages/Search.js
+++ frontend/src/Pages/Search.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Cards from 'Components/Cards';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import { Link } from 'react-router-dom';
 
@@ -9,6 +9,8 @@ const Search = (props) => {
   const [data, setData] = useState(null);
   const [total, setTotal] = useState(null);
   const pageNum = page ? page : 1;
+  const pageLimit = 16;
+  const navigate = useNavigate();
 
   const countTotal = async () => {
     const check = await fetch(`/home/location/${query}`);
@@ -20,7 +22,7 @@ const Search = (props) => {
   const [multipleMarkers, setMutipleMarkers] = useState(null)
 
   const fetchData = async () => {
-    const resp = await fetch(`/home/location/${query}?limit=3&page=${pageNum}`);
+    const resp = await fetch(`/home/location/${query}?limit=${pageLimit}&page=${pageNum}`);
     const json = await resp.json();
     setData(json);
   }
@@ -43,21 +45,29 @@ const Search = (props) => {
   }
 
   useEffect(() => {
-    setData(null);
-    fetchData();
     countTotal();
+    getMarkers();
   }, [query]);
 
   useEffect(() => {
-    getMarkers()
-  }, [data]);
+    fetchData();
+  }, [page]);
 
   const loaded = () => {
 
-    const pageCount = Math.floor(total/3);
-    console.log(pageCount);
+    const pageCount = Math.ceil(total/pageLimit);
+    console.log('total',total,pageCount);
     const pageArr = new Array(pageCount).fill('');
-    const pageList = pageArr.map((i,idx) => { return <a href={`/search/${query}/${idx+1}`}>{idx+1}</a>  });
+    const pageList = pageArr.map((i,idx) => { 
+      return (
+        <>
+          <a onClick={() => {navigate(`/search/${query}/${idx+1}`)}} key={idx} style={{cursor:'pointer'}}>{idx+1}</a> 
+          {
+            idx !== pageArr.length-1 && <>&nbsp; | &nbsp;</>
+          }
+        </>
+      )
+    });
 
     return (
       <div className="abnb-list-main-cont">
@@ -66,24 +76,26 @@ const Search = (props) => {
         <div className="abnb-main-cont mt-5">
           <div className="main-list">
             <Cards data={data} /> 
-            <MapContainer center={data ? [data[0].location.lat, data[0].location.long] : ''} zoom={10} scrollWheelZoom={true}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {multipleMarkers}
-
-          </MapContainer>
           </div>
-          <div className="text-center mt-3">
-            Pages | {pageList}
+          <div className="text-center mt-3 pt-4" style={{borderTop:'1px solid #efefef'}}>
+            Pages: &nbsp; &nbsp; {pageList}
+          </div>
+          <div className="mt-5">
+            <MapContainer center={data ? [data[0].location.lat, data[0].location.long] : ''} zoom={10} scrollWheelZoom={true}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              {multipleMarkers}
+
+            </MapContainer>
           </div>
         </div>
       </div>
     )
   }
 
-  if (data && data.length > 0) {
+  if (data && data.length > 0 && total) {
     return loaded();
   } else if (data && data.length === 0) {
     return (
